cd703bf14b9ac2bdd0f4d373b659564a198df24c
Luis Rodriguez Lujan <luis.rodriguez@piperlab.es>
2018-09-14 10:45:48 +0200

    Add catboost engine

--- R/boost_tree_data.R
+++ R/boost_tree_data.R
@@ -2,6 +2,7 @@
 boost_tree_arg_key <- data.frame(
   xgboost = c("max_depth", "nrounds", "eta", "colsample_bytree", "min_child_weight", "gamma", "subsample"),
   C5.0 =    c(         NA,  "trials",    NA,                NA,         "minCases",       NA,    "sample"),
+  catboost =c("depth", "iterations", "learning_rate", "rsm", NA, NA, NA),
   stringsAsFactors = FALSE,
   row.names =  c("tree_depth", "trees", "learn_rate", "mtry", "min_n", "loss_reduction", "sample_size")
 )
@@ -11,6 +12,7 @@ boost_tree_modes <- c("classification", "regression", "unknown")
 boost_tree_engines <- data.frame(
   xgboost =    rep(TRUE, 3),
   C5.0    =    c(            TRUE,        FALSE,      TRUE),
+  catboost =   rep(TRUE, 3),
   row.names =  c("classification", "regression", "unknown")
 )
 
@@ -134,6 +136,126 @@ C5.0_train <- function(x, y, weights = NULL, trials = 15, minCases = 2, sample =
   eval_tidy(fit_call)
 }
 
+
+# Catboost definition -----------------------------------------------------
+
+catboost_train <- function(
+  x, y,
+  depth = 6, iterations = 500, learning_rate  = 0.03, rsm = 1,
+  logging_level = 'Silent', thread_count = 1, od_pval = 1E-3, ...) {
+
+  if (is.numeric(y)) {
+    loss <- "RMSE"
+    eval_loss <- "RMSE"
+  } else {
+
+    if(is.logical(y)){
+      lvl <- c(F,T)
+      dif <- 0
+    } else {
+      lvl <- levels(y)
+      dif <- 1
+    }
+
+    y <- as.integer(y) - dif
+
+    if (length(lvl) == 2) {
+      loss <- "Logloss"
+      eval_loss <- "AUC"
+    } else {
+      loss <- "CrossEntropy"
+      eval_loss <- "MultiClass"
+    }
+  }
+
+  n <- nrow(x)
+  p <- ncol(x)
+
+  x <- catboost::catboost.load_pool(x, label = y, feature_names = as.list(colnames(x)) )
+
+  # `colsample_bytree` to be on (0, 1] if not
+
+  if(rsm > 1)
+    rsm <- rsm/p
+  rsm <- min(1, rsm)
+
+  arg_list <- list(
+    depth = depth,
+    iterations = iterations,
+    learning_rate = learning_rate,
+    rsm = rsm,
+    loss_function = loss,
+    eval_metric = eval_loss,
+    allow_writing_files = FALSE,
+    logging_level = logging_level,
+    thread_count = thread_count,
+    od_pval = od_pval)
+
+  # eval if contains expressions?
+  others <- list(...)
+
+  # Not really proud of this.
+  if (all(c("test_data","test_label") %in% others)) {
+    if(loss != "RMSE"){
+      test_label <- as.integer(others$test_label) - dif
+    }
+
+    tpool <- catboost::catboost.load_pool(as.data.frame(others$test_data)[,colnames(x)],
+                                          label = test_label,
+                                          feature_names = as.list(colnames(x)) )
+  } else {
+    tpool <- NULL
+  }
+
+
+  others <-
+    others[!(names(others) %in% c("learn_pool", "test_pool", "test_data", "test_label",
+                                  "subsample", names(arg_list)))]
+
+  main_args <- list(
+    learn_pool = quote(x),
+    test_pool = quote(tpool),
+    params = arg_list)
+
+  call <- make_call(fun = "catboost.train", ns = "catboost", main_args)
+
+  # override or add some other args
+  if (length(others) > 0)
+    for (i in names(others))
+      call[[i]] <- others[[i]]
+
+  ret <- eval_tidy(call, env = rlang::current_env())
+
+  if (loss != "RMSE") {
+    ret$levels <- lvl
+  }
+
+  ret
+}
+
+catboost_pred <- function(object, newdata, pred_type, ...) {
+
+  newdata <- catboost::catboost.load_pool(newdata, feature_names = as.list(colnames(newdata)))
+
+  res <- catboost::catboost.predict(object, newdata, verbose = FALSE,
+                                    prediction_type = pred_type, ...)
+
+  if(pred_type == "Probability"){
+    if (!is.data.frame(res) & !is.matrix(res)){
+      res <- cbind( res, 1-res)
+    }
+
+    res <- as.data.frame(res)
+    colnames(res) <- object$levels
+  } else if (pred_type == "Class") {
+    res <- object$levels[res+1]
+  }
+
+  res
+
+}
+
+
 ###################################################################
 
 
@@ -248,3 +370,56 @@ boost_tree_C5.0_data <-
     )
   )
 
+boost_tree_catboost_data <-
+  list(
+    libs = "catboost",
+    fit = list(
+      interface = "data.frame",
+      protect = c("x", "y"),
+      func = c(pkg = NULL, fun = "catboost_train"),
+      defaults = list()
+    ),
+    pred = list(
+      pre = NULL,
+      post = NULL,
+      func = c(fun = "catboost_pred"),
+      args =
+        list(
+          object = quote(object$fit),
+          newdata = quote(new_data),
+          pred_type = "RawFormulaVal"
+        )
+    ),
+    classes = list(
+      pre = NULL,
+      post = NULL,
+      func = c(pkg = NULL, fun = "catboost_pred"),
+      args =
+        list(
+          object = quote(object$fit),
+          newdata = quote(new_data),
+          pred_type = "Class"
+        )
+    ),
+    prob = list(
+      pre = NULL,
+      post = NULL,
+      func = c(pkg = NULL, fun = "catboost_pred"),
+      args =
+        list(
+          object = quote(object$fit),
+          newdata = quote(new_data),
+          pred_type = "Probability"
+        )
+    ),
+    raw = list(
+      pre = NULL,
+      func = c(fun = "catboost_pred"),
+      args =
+        list(
+          object = quote(object$fit),
+          newdata = quote(new_data),
+          pred_type = "RawFormulaVal"
+        )
+    )
+  )
